48935f8277be4379be39197b206b1b68b0ef9a94
2di-crypto <dirobapulga@gmail.com>
2025-04-01 12:26:42 +0000

    entrada de dados

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,37 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    printf("****JOGO DE XADRES****\n");
+    
+    printf("\n");
+// mover a torre 5 casas a direita usando loop for
+printf("**** TORRE ****\n");
+for (int i = 0; i < 5; i++) {
+    printf("DIREITA\n");// imprime a direçao do movimento
+}
+printf("\n");
+int a = 1;
+int b = 1;
+
+// mover o bispo 5 casas na diagonal (cima direita) usando loop while
+printf("**** BISPO ****\n");
+while (a <= 5){
+    printf("DIAGONAL(CIMA DIREITA)\n");//imprime a direçao do movimento
+ 
+    a++;///incremento impede o loop infinito 
+}
+printf("\n");
+
+/// MOVER A RAINHA 8 CASAS A ESQUERDA usando loop do-while
+printf("**** RAINHA****\n");
+
+do{
+    printf("ESQUERDA\n");///imprime a direçao do movimento
+    b++; //incremento impede o loop infinito
+}while(b  <= 8);
+
+printf("\n");
+
+printf("**** XEQUE-MATE ****\n");
     return 0;
 }
